8242b875bb2f81aee05be0fa251e8349ece0527c
Simone Pesce <simone.pesce93@gmail.com>
2025-03-29 18:52:28 +0100

    fix logo

--- frontend/nextjs/components/Header.tsx
+++ frontend/nextjs/components/Header.tsx
@@ -20,7 +20,7 @@ const Header = ({ loading, isStopped, showResult, onStop, onNewResearch }: Heade
           {/* Logo/Home link */}
           <a href="/">
             <Image
-              src="/img/logo-footer-scientifica.png"
+              src="/img/venture-capital_negative.png"
               alt="logo"
               width={60}
               height={60}
